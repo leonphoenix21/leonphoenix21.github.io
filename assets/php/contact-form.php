197e46c4081c30508c059ac09a6090478a6e9184
leonphoenix21 <noelmutiadev@gmail.com>
2025-01-19 04:49:49 -0600

    Added Achievements

--- assets/php/contact-form.php
+++ assets/php/contact-form.php
@@ -2,7 +2,7 @@
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-    $to = 'your-email@gmail.com'; // <-- Enter your E-Mail address here.
+    $to = 'noelmutiadev@gmail.com'; // <-- Enter your E-Mail address here.
     $subject = $_POST['subject'];
 
     $body = "From: $name <br> E-Mail: $email <br> Message: <br> $message";
@@ -12,9 +12,11 @@
     $headers .= 'From:' . $email. "\r\n";
     $headers .= 'Cc:' . $email. "\r\n";
 
-    if (mail($to, "New Message from Website: $subject", $body, $headers)) {
-        echo json_encode(['status' => 'success', 'message' => 'Message sent successfully.']);
+    if (!mail($to, "New Message from Website: $subject", $body, $headers)) {
+        error_log("Mail failed: " . print_r(error_get_last(), true)); // Log error details
+        echo json_encode(['status' => 'error', 'message' => 'Message could not be sent.']);
     } else {
-        echo json_encode(['status' => 'error', 'message' => 'Message could not be sent. Please try again later.']);
+        echo json_encode(['status' => 'success', 'message' => 'Message sent successfully.']);
     }
+    
 ?>
